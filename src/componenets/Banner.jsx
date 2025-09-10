3d70d7e028b07185a5f037be0c8e8793e220fa05
ankitakolekar <ankitakolekar418@gmail.com>
2025-09-10 14:50:49 +0530

    update

--- src/componenets/Banner.jsx
+++ src/componenets/Banner.jsx
@@ -73,6 +73,76 @@
 // export default Banner;
  
 
+// import React from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/autoplay";
+// import { Pagination, Autoplay } from "swiper/modules";
+
+// import sliderone from "../assets/images/sliderone.jpg";
+// import slidertwo from "../assets/images/slidertwo.jpeg";
+// import sliderthree from "../assets/images/sliderthree.png";
+// import banner_one from "../assets/images/banner_one.jpg";
+// import banner_two from "../assets/images/banner_two.png";
+// import banner_three from "../assets/images/banner_three.jpg";
+
+// function Banner() {
+//   return (
+//     <>
+//       {/* 🔹 Slider Section */}
+//   <div className="h-[250px] sm:h-[350px] md:h-[475px] w-full max-w-full overflow-hidden">
+//         <a
+//           href="https://www.nakbook.com/product-category/vanrakshak/"
+//           tabIndex={0}
+//         >
+//           <Swiper
+//             loop={true}
+//             speed={1000}
+//             autoplay={{
+//               delay: 3000,
+//               disableOnInteraction: false,
+//             }}
+//             pagination={{
+//               dynamicBullets: true,
+//             }}
+//             modules={[Pagination, Autoplay]}
+//             className="w-full h-full"
+//           >
+//             <SwiperSlide>
+//               <img
+//                 src={sliderone}
+//                 className="w-full h-full object-cover rounded-lg"
+//                 alt="slider"
+//               />
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <img
+//                 src={slidertwo}
+//                 className="w-full h-full object-cover rounded-lg"
+//                 alt="slider"
+//               />
+//             </SwiperSlide>
+//             <SwiperSlide>
+//               <img
+//                 src={sliderthree}
+//                 className="w-full h-full object-cover rounded-lg"
+//                 alt="slider"
+//               />
+//             </SwiperSlide>
+//           </Swiper>
+//         </a>
+//       </div>
+
+//       {/* 🔹 Banner Cards */}
+      
+//     </>
+//   );
+// }
+
+// export default Banner;
+
+
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -83,15 +153,12 @@ import { Pagination, Autoplay } from "swiper/modules";
 import sliderone from "../assets/images/sliderone.jpg";
 import slidertwo from "../assets/images/slidertwo.jpeg";
 import sliderthree from "../assets/images/sliderthree.png";
-import banner_one from "../assets/images/banner_one.jpg";
-import banner_two from "../assets/images/banner_two.png";
-import banner_three from "../assets/images/banner_three.jpg";
 
 function Banner() {
   return (
     <>
       {/* 🔹 Slider Section */}
-  <div className="h-[250px] sm:h-[350px] md:h-[475px] w-full max-w-full overflow-hidden">
+      <div className="w-full max-w-full overflow-hidden mb-6">
         <a
           href="https://www.nakbook.com/product-category/vanrakshak/"
           tabIndex={0}
@@ -112,30 +179,27 @@ function Banner() {
             <SwiperSlide>
               <img
                 src={sliderone}
-                className="w-full h-full object-cover rounded-lg"
+                className="w-full h-auto object-cover rounded-lg"
                 alt="slider"
               />
             </SwiperSlide>
             <SwiperSlide>
               <img
                 src={slidertwo}
-                className="w-full h-full object-cover rounded-lg"
+                className="w-full h-auto object-cover rounded-lg"
                 alt="slider"
               />
             </SwiperSlide>
             <SwiperSlide>
               <img
                 src={sliderthree}
-                className="w-full h-full object-cover rounded-lg"
+                className="w-full h-auto object-cover rounded-lg"
                 alt="slider"
               />
             </SwiperSlide>
           </Swiper>
         </a>
       </div>
-
-      {/* 🔹 Banner Cards */}
-      
     </>
   );
 }
